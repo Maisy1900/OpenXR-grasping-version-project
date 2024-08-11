9513a41bb0c7ea93292f7617e71485173e3388b0
Maisy1900 <mxr536@student.bham.ac.uk>
2024-08-11 18:55:42 +0100

    thinking about genetic algorithm

--- Assets/MainExperimentsetup.cs
+++ Assets/MainExperimentsetup.cs
@@ -38,10 +38,14 @@ public class MainExperimentsetup : MonoBehaviour
 {
     // Variables to store experiment data
     DataClass dataclass;
-    public int number_of_simulations = 10;
+    public int number_of_simulation_blocks = 5; // Number of full sets of 9 trials
+    private int number_of_simulations;
     public GameObject articulationObject;
     ArticulationDriver_v2 articulationDriver;
     public Transform cubeObj;
+    public StackingTrial stack;
+    public liftingTrial lift;
+    public PushingTrial pushing; 
 
     public Animation[] animationz; // 0 = Calibration anim, 1-3 = lift, 4-6 = push and 7-9 = stack
 
@@ -70,6 +74,33 @@ public class MainExperimentsetup : MonoBehaviour
         // Clear all lists 
         dataclass.ClearAllLists(); 
     }
+    void Start()
+    {
+        // Calculate the total number of simulations
+        number_of_simulations = number_of_simulation_blocks * 9;
+
+        // Skip the first animation (calibration) and start from index 1
+        int originalCount = animationz.Length - 1; // Since we skip the first one
+
+        // Create a list to hold the expanded array of animations
+        List<Animation> expandedAnimations = new List<Animation>();
+
+        // Duplicate the original animations (excluding the first one) to fill the required number of trials
+        for (int i = 0; i < number_of_simulations; i++)
+        {
+            // Add the animations starting from index 1 to ignore calibration
+            expandedAnimations.Add(animationz[(i % originalCount) + 1]);
+        }
+
+        // Convert the list back to an array and assign it to the animationz variable
+        animationz = expandedAnimations.ToArray();
+
+        // Now shuffle this expanded array
+        Shuffle(animationz);
+
+        // Start the trials
+        StartCoroutine(ConductTrials());
+    }
 
     // Function to conduct trials
     IEnumerator ConductTrials()
@@ -77,45 +108,69 @@ public class MainExperimentsetup : MonoBehaviour
 
         RunHandCalibration();
         yield return new WaitForSeconds(1.0f);
-
-        for (int i = 0; i<number_of_simulations; i++)
+        for (int i = 0; i < number_of_simulations; i++)
         {
-            // Setup scene (cubes etc) 
-            //shuffledTrials[[i]
-
-            // TODO: Change physics parameters here based on the defined metric
+            // Setup physics parameters for this trial
             Physics.defaultSolverIterations = 10; // [3-40] in step size of 1
             Physics.defaultSolverVelocityIterations = 5; // [1-40]
             Physics.defaultContactOffset = 0.01f; // [0.001,0.1]
             Physics.defaultMaxDepenetrationVelocity = 10; // [1-100]
             Physics.bounceThreshold = 2; // [0.1-4]
 
-            // TODO: Run hand animations for each trial per condition (lift, push and stack) 
-            
-            
-            // Play animation 
-            animationz[shuffledTrials[i]].Play();
-
-            // Wait for trial to complete
-            while(animationz[shuffledTrials[i]].isPlaying)
+            // Determine which trial type to run based on the current animation index
+            if (i % 9 >= 0 && i % 9 <= 2)  // Lift animations (1-3)
             {
-                //RecordResults(Vector3 cubePosition, Vector3 handPose, float timing, int trialNumber); 
-                yield return null; 
+                lift.enabled = true;
+                pushing.enabled = false;
+                stack.enabled = false;
+
+                lift.RunTrial();
             }
-            animationz[shuffledTrials[i]].Stop();
+            else if (i % 9 >= 3 && i % 9 <= 5)  // Push animations (4-6)
+            {
+                lift.enabled = false;
+                pushing.enabled = true;
+                stack.enabled = false;
 
+                pushing.RunTrial();
+            }
+            else if (i % 9 >= 6 && i % 9 <= 8)  // Stack animations (7-9)
+            {
+                lift.enabled = false;
+                pushing.enabled = false;
+                stack.enabled = true;
 
-            // TODO: Compute results after each trial (this means calculating the distances between the current position and the recorded (animation) position of the cube)
+                stack.RunTrial();
+            }
 
+            // Play the corresponding animation
+            animationz[i].Play();
 
-            // Update optimization so that the algorithm can decide how to change physics parameters
+            // Wait for the animation to complete
+            while (animationz[i].isPlaying)
+            {
+                yield return null;
+            }
 
+            animationz[i].Stop();
 
-            // Record data into a file (not essential)
-            SaveDataFile();
-
-
+            // Optionally, add a short delay between trials
+            yield return new WaitForSeconds(1.0f);
         }
+    // Play animation 
+    //animationz[shuffledTrials[i]].Play();
+
+
+
+
+    // TODO: Compute results after each trial (this means calculating the distances between the current position and the recorded (animation) position of the cube)
+
+
+    // Update optimization so that the algorithm can decide how to change physics parameters
+
+
+    // Record data into a file (not essential)
+    SaveDataFile();
 
         yield return null; 
 
@@ -142,40 +197,22 @@ public class MainExperimentsetup : MonoBehaviour
         // Calibration logic is over
         Debug.Log("Calibration is over");
     }
-
-
-    // Function to run hand animations for each trial
-    void RunHandAnimations()
-    {
-        // TODO: Implement hand animations here
-    }
-    void Start()
-    {
-        // Reference the Animator component
-
-        // Find the ArticulationDriver script on the specified GameObject
-        articulationDriver = articulationObject.GetComponent<ArticulationDriver_v2>();
-
-        // Start the calibration coroutine
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // TODO: Any per-frame updates
     }
 
     private System.Random _random = new System.Random();
-    void Shuffle(int[] array)
+    void Shuffle(Animation[] array)
     {
         int p = array.Length;
         for (int n = p - 1; n > 0; n--)
         {
             int r = _random.Next(0, n);
-            int t = array[r];
+            Animation t = array[r];
             array[r] = array[n];
             array[n] = t;
         }
     }
+
 
 }
